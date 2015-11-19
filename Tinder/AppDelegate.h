e299473c266282a1074ed31ed261f04a18917d6b
Florent Bonomo <florent@groupon.com>
2015-11-19 14:42:27 -0800

    Initial Commit

---
+++ Tinder/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Tinder
+//
+//  Created by Florent Bonomo on 11/19/15.
+//  Copyright © 2015 flochtililoch. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
